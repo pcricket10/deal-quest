9e2c99127e816bf7f1c8d81ed043b370c80bc953
Philip McCrickard <pcricket10@gmail.com>
2022-03-31 17:36:19 -0400

    user login

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,6 +1,17 @@
 // backend/routes/api/index.js
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js')
 
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+router.post('/test', (req, res) => {
+  res.json({ requestBody: req.body });
+});
+
+/********************API TESTS******************************/
 // const asyncHandler = require('express-async-handler');
 // const { setTokenCookie } = require('../../utils/auth.js');
 // const { User } = require('../../db/models/');
@@ -30,8 +41,6 @@ const router = require('express').Router();
 //   return res.json({ user })
 // }));
 
-// router.post('/test', (req, res) => {
-//   res.json({ requestBody: req.body });
-// });
+
 
 module.exports = router;
