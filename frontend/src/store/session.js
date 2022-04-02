39dfd16f4381838a17a0b4496f949d234655cfb6
Philip McCrickard <pcricket10@gmail.com>
2022-04-02 14:08:15 -0400

    signup actions

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -17,7 +17,20 @@ const removeUser = () => {
   };
 };
 
-export const login = (user) => async (dispatch) => {
+export const signup = user => async dispatch => {
+  const { username, email, password } = user;
+  const response = await csrfFetch('/api/users', {
+    method: 'POST',
+    body: JSON.stringify({
+      username, email, password
+    }),
+  });
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
+}
+
+export const login = user => async dispatch => {
   const { credential, password } = user;
   const response = await csrfFetch('/api/session', {
     method: 'POST',
